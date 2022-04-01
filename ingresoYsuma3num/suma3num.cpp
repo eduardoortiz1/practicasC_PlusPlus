465bd86e531e0a6020320dcc7c1ca85cb2a7328d
Eduardo Ortiz <eduardo.ortiz14@gmail.com>
2022-04-01 12:08:05 -0500

    Completado ejercicio 2

---
+++ ingresoYsuma3num/suma3num.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+    int numero1,numero2,numero3,suma;
+    
+    cin >> numero1 >> numero2 >> numero3;
+
+    suma = numero1+numero2+numero3;
+
+    cout << suma << endl;
+    
+    return 0;
+}
